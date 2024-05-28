65b49c3b60a3bd8f9441e718effbfea17c69de0b
Hamza Yaqoob <hamzayaqoob127@gmail.com>
2024-05-28 13:02:04 +0500

    Initial commit

---
+++ Assigenment function/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Assigenment_function
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbloutput = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbinput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bell MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 24);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Result";
+            // 
+            // lbloutput
+            // 
+            this.lbloutput.BackColor = System.Drawing.Color.White;
+            this.lbloutput.Location = new System.Drawing.Point(98, 204);
+            this.lbloutput.Name = "lbloutput";
+            this.lbloutput.Size = new System.Drawing.Size(262, 39);
+            this.lbloutput.TabIndex = 10;
+            this.lbloutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bell MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(97, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Enter Number For Square";
+            // 
+            // tbinput
+            // 
+            this.tbinput.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbinput.Location = new System.Drawing.Point(101, 97);
+            this.tbinput.Multiline = true;
+            this.tbinput.Name = "tbinput";
+            this.tbinput.Size = new System.Drawing.Size(259, 39);
+            this.tbinput.TabIndex = 8;
+            this.tbinput.TextChanged += new System.EventHandler(this.tbinput_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(155, 267);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 41);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Square";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Assigenment_function.Properties.Resources.wallpaperflare_com_wallpaper__3_;
+            this.ClientSize = new System.Drawing.Size(501, 340);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbloutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbinput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbloutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbinput;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Assigenment function/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assigenment_function
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool IsNumeric(string input)
+        {
+            foreach (char c in input)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private void tbinput_TextChanged(object sender, EventArgs e)
+        {
+            if (!IsNumeric(tbinput.Text))
+            {
+                MessageBox.Show("Please enter only numeric digits!");
+                tbinput.Text = tbinput.Text.Remove(tbinput.Text.Length - 1);
+                tbinput.SelectionLength = tbinput.Text.Length;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tbinput.Text != string.Empty)
+            {
+                string num = tbinput.Text;
+                double result = Convert.ToDouble(num);
+                result = result * result;
+                lbloutput.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Enter The Number");
+            }
+        }
+    }
+}
